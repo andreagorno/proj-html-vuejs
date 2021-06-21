10c8f948468b888fc2c9bce04b92b500a8cc3137
andreagorno <andreagorno98@gmail.com>
2021-06-21 14:58:00 +0200

    scheletro pagina web

---
+++ src/components/Header.vue
@@ -0,0 +1,85 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/img/logo-img-01.png" alt="logo_small">
+        </div>
+        <div class="nav">
+            <div 
+            v-for="(option) in options"
+            :key="option.id"
+            >
+                <HeaderMenu 
+                    :item="option"
+                />  
+            </div>
+            <div>
+                <img src="../assets/svg/svg-1.svg">
+            </div>
+        </div>
+        
+            
+    </header>
+</template>
+
+<script>
+import HeaderMenu from "./HeaderMenu.vue"
+
+export default {
+    name: "Header",
+
+    components: {
+        HeaderMenu
+    },
+
+    data: function () {
+    return {
+      options: [
+        {
+          title: "Home",
+          id: 1 
+        },
+        {
+          title: "Pages",
+          id: 2
+        },
+        {
+          title: "Portfolio",
+          id: 3
+        },
+        {
+          title: "Blog",
+          id: 4 
+        },
+        {
+          title: "Shop",
+          id: 5 
+        },
+        {
+          title: "Elements",
+          id: 6 
+        }
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        display: flex;
+        align-items: center;
+        padding: 30px;
+
+        .logo {
+            flex-grow: 1;
+            
+            img {
+                height: 30px;
+            }
+        }
+
+        .nav {
+            display: flex;
+        }
+    }
+</style>
